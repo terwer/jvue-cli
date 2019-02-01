9438618110efc6075afe4495bd1d50b3a3c0a714
Terwer <youweics@sina.com>
2019-02-01 12:54:01 +0800

    Add index

--- src/main/webapp/src/router.js
+++ src/main/webapp/src/router.js
@@ -1,15 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router-nashorn";
-import Home from "./views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "index",
+    component: () => import(/* webpackChunkName: "home" */ "./views/Index.vue")
   },
+  { path: "/home", redirect: { name: "index" } },
   {
     path: "/about",
     name: "about",
